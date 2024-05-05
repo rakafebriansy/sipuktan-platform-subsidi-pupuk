f4f8074574ef0333c4394c1ed3052d0c231e798a
rakafebriansy <kerenr445@gmail.com>
2024-05-05 21:33:01 +0700

    fix: text center in faq

--- resources/views/homepage/pages/index.blade.php
+++ resources/views/homepage/pages/index.blade.php
@@ -110,7 +110,7 @@
       @foreach ($faqs_kios_resmi as $faq_kios_resmi)
         <h2 id="accordion-flush2-heading-{{ $faq_kios_resmi->id }}">
           <button type="button" class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush2-body-{{ $faq_kios_resmi->id }}" aria-expanded="true" aria-controls="accordion-flush2-body-{{ $faq_kios_resmi->id }}">
-            <span>{{ $faq_kios_resmi->pertanyaan }}</span>
+            <span class=" text-start">{{ $faq_kios_resmi->pertanyaan }}</span>
             <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
             </svg>
