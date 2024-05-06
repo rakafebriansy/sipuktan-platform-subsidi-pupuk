028c149d81f1ccdf1a03f504763c796f75e9a58f
rakafebriansy <kerenr445@gmail.com>
2024-05-06 15:56:32 +0700

    feat: adding custom messages for login petani

--- app/Http/Requests/PetaniLoginRequest.php
+++ app/Http/Requests/PetaniLoginRequest.php
@@ -23,8 +23,21 @@ class PetaniLoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'nik' => ['required','numeric','min:16'],
-            'kata_sandi' => ['required','min:6'],
+            'nik' => 'required|numeric|min:16',
+            'kata_sandi' => 'required|min:6',
+        ];
+    }
+        
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'nik.required' => 'NIK tidak boleh kosong',
+            'kata_sandi.required' => 'Kata sandi tidak boleh kosong',
         ];
     }
 }
