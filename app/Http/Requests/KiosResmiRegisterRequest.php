e5a77f27df8d4b84e143df6e1a1fe442512d55b3
rakafebriansy <kerenr445@gmail.com>
2024-05-06 16:02:13 +0700

    feat: adding custom messages for register petani

--- app/Http/Requests/KiosResmiRegisterRequest.php
+++ app/Http/Requests/KiosResmiRegisterRequest.php
@@ -23,13 +23,13 @@ class KiosResmiRegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'nib' => 'required|numeric|min:13',
+            'nib' => 'required|numeric|min:13|unique:kios_resmis,nib',
             'kata_sandi' => 'required|min:6',
             'nama'=> 'required',
             'jalan'=> 'required',
             'id_kecamatan' => 'required',
             'foto_ktp' => 'required|mimes:png,jpg|max:5120',
-            'nik' => 'required|numeric|min:16',
+            'nik' => 'required|numeric|min:16|unique:pemilik_kios,nik',
             'nama_pemilik' => 'required',
             'nomor_telepon' => 'required|numeric',
         ];
@@ -44,10 +44,11 @@ class KiosResmiRegisterRequest extends FormRequest
     {
         return [
             'nib.required' => 'NIB tidak boleh kosong',
+            'nib.unique:kios_resmis,nib' => 'NIB telah terdaftar',
             'nib.numeric' => 'NIB harus berupa angka',
-            'nib.min:13' => 'NIB harus harus berjumlah minimal 13 karakter',
+            'nib.min:13' => 'NIB harus berjumlah minimal 13 karakter',
             'kata_sandi.required' => 'Kata sandi tidak boleh kosong',
-            'kata_sandi.min:6' => 'Kata sandi harus harus berjumlah minimal 6 karakter',
+            'kata_sandi.min:6' => 'Kata sandi harus berjumlah minimal 6 karakter',
             'nama.required' => 'Nama kios tidak boleh kosong',
             'jalan.required' => 'Alamat tidak boleh kosong',
             'id_kecamatan.required' => 'Kecamatan tidak boleh kosong',
@@ -56,7 +57,8 @@ class KiosResmiRegisterRequest extends FormRequest
             'foto_ktp.max:5120' => 'Foto KTP harus memiliki ukuran kurang dari 5MB',
             'nik.required' => 'NIK tidak boleh kosong',
             'nik.numeric' => 'NIK harus berupa angka',
-            'nik.min:13' => 'NIK harus harus berjumlah minimal 16 karakter',
+            'nik.unique:pemilik_kios,nik' => 'NIK telah terdaftar',
+            'nik.min:13' => 'NIK harus berjumlah minimal 16 karakter',
             'nama_pemilik.required' => 'Nama tidak boleh kosong',
             'nomor_telepon.required' => 'Nomor telepon tidak boleh kosong',
             'nomor_telepon.numeric' => 'Nomor telepon harus berupa angka',
--- app/Http/Requests/PetaniRegisterRequest.php
+++ app/Http/Requests/PetaniRegisterRequest.php
@@ -23,12 +23,37 @@ class PetaniRegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'nik' => ['required','min:16','numeric','unique:petanis,nik'],
-            'nama' => ['required','max:60','min:3'],
-            'kata_sandi' => ['required','min:6'],
-            'foto_ktp' => ['required','mimes:png,jpg','max:5140'],
-            'nomor_telepon' => ['required','numeric'],
+            'nik' => 'required|min:16|numeric|unique:petanis,nik',
+            'nama' => 'required|max:60',
+            'kata_sandi' => 'required|min:6',
+            'foto_ktp' => 'required|mimes:png,jpg|max:5120',
+            'nomor_telepon' => 'required|numeric',
             'id_kelompok_tani' => 'required'
+        ];
+    }
+            
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'nik.required' => 'NIK tidak boleh kosong',
+            'nik.numeric' => 'NIK harus berupa angka',
+            'nik.unique:petanis,nik' => 'NIK telah terdaftar',
+            'nik.min:16' => 'NIK harus berjumlah minimal 16 karakter',
+            'nama.required' => 'Nama tidak boleh kosong',
+            'nik.max:60' => 'Nama harus harus berjumlah maksimal 60 karakter',
+            'kata_sandi.required' => 'Kata sandi tidak boleh kosong',
+            'kata_sandi.min:6' => 'Kata sandi harus berjumlah minimal 6 karakter',
+            'foto_ktp.required' => 'Foto KTP tidak boleh kosong',
+            'foto_ktp.mimes:png,jpg' => 'Foto KTP harus berekstensi .png atau .jpg',
+            'foto_ktp.max:5120' => 'Foto KTP harus memiliki ukuran kurang dari 5MB',
+            'nomor_telepon.required' => 'Nomor telepon tidak boleh kosong',
+            'nomor_telepon.numeric' => 'Nomor telepon harus berupa angka',
+            'id_kelompok_tani.required' => 'Kelompok tani tidak boleh kosong'
         ];
     }
 }
