62b12fec63d55e9f664a8ffd5fd110b310cf944a
rakafebriansy <kerenr445@gmail.com>
2024-05-08 10:24:02 +0700

    feat: adding show old value in buat keluhan petani

--- resources/views/dashboard/petani/pages/keluhan.blade.php
+++ resources/views/dashboard/petani/pages/keluhan.blade.php
@@ -90,11 +90,11 @@
                 <div class="mb-4">
                     <div class="mb-5">
                         <label for="subjek" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Subjek</label>
-                        <input type="text" name="subjek" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"  />
+                        <input type="text" value="{{ old('subjek','') }}" name="subjek" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"  />
                     </div>
                     <div class="mb-2">
                         <label for="keluhan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Keluhan</label>
-                        <textarea name="keluhan" rows="3" class="block p-2.5 w-full text-sm bg-gray-50 text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" style="resize: none" placeholder="Jelaskan keluhan anda..."></textarea>
+                        <textarea name="keluhan" rows="3" class="block p-2.5 w-full text-sm bg-gray-50 text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" style="resize: none" placeholder="Jelaskan keluhan anda...">{{ old('keluhan','') }}</textarea>
                     </div>
                 </div>
                 <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
