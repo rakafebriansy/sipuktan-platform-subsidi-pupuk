9de6c2b7255ccc5916f3d3abb07514291b231443
rakafebriansy <kerenr445@gmail.com>
2024-05-12 19:16:02 +0700

    fix: alokasi petani view

---
+++ resources/views/bot/registrasiweb.blade.php
@@ -0,0 +1,8 @@
+1. Siapkan berkas yang diperlukan untuk registrasi, pastikan Anda sudah terdaftar sebagai anggota kelompok tani (untuk petani).
+2. Buka situs resmi SIPUKTAN (link)
+3. Klik “Daftar Sekarang!”
+4. Isikan data diri atau data kios resmi dengan sesuai dan unggah foto KTP.
+5. Klik tombol “Daftar”
+6. Silakan tunggu verifikasi untuk registrasi akun.
+Untuk mengecek verifikasi akun Anda, silakan mencoba login secara berkala. Apabila terdapat kesulitan dalam registrasi, dimohon untuk menghubungi pengurus kelompok tani masing-masing.
+/menu
